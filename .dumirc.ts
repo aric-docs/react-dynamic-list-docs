e456b081d05b44a7650ff58d3a519b9b3b7a06f5
afeiship <1290657123@qq.com>
2026-04-17 21:16:05 +0800

    feat: init

---
+++ .dumirc.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'dumi';
+import * as path from 'node:path';
+
+export default defineConfig({
+  alias: {
+    '@builtins': path.resolve(__dirname, '.dumi/theme/builtins'),
+  },
+  base: '/react-dynamic-list-docs/',
+  publicPath: '/react-dynamic-list-docs/',
+  logo: '/react-dynamic-list-docs/logo.png',
+  locales: [{ id: 'en-US', name: 'English' }],
+  resolve: {
+    codeBlockMode: 'passive',
+  },
+  themeConfig: {
+    name: 'react-dynamic-list-docs',
+    description: 'react-dynamic-list docs.',
+    nav: [
+      {
+        title: 'Guide',
+        link: '/guide/getting-started',
+      },
+      {
+        title: 'Components',
+        link: '/components',
+      },
+      {
+        title: 'Playground',
+        link: '/playground',
+      },
+    ],
+    nprogress: true,
+    socialLinks: {
+      github: 'https://github.com/aric-tpls/react-dynamic-list-docs',
+    },
+  },
+  plugins: ['@umijs/plugins/dist/tailwindcss'],
+  tailwindcss: {},
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+# react-dynamic-list-docs
+
+> react-dynamic-list docs.
+
+## preview
+
+- https://aric-docs.github.io/react-dynamic-list-docs/
